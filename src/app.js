8d6e3e64eea1ba4e792422f7d68884647472a5d9
Ayush <ayush1240945@gmail.com>
2022-10-16 12:46:48 +0545

    hosting

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 8000
+const path = require('path');
+
+const staticPath = path.join(__dirname,"../public");
+app.use(express.static(staticPath));
+app.set('view engine','hbs');
+
+//routing
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
+app.get("/weather",(req,res)=>{
+    res.render("Welcome to weather page");
+})
+app.get("*",(req,res)=>{
+    res.render("error 404 check name correctly ");
+})
+
+app.listen(port,()=>{
+    console.log(`Listening to the port ${port}`);
+})
